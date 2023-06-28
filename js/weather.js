73613d19aa0ba64f0da0302bf58340a8eb552773
Aislã Lucio <36649051+aisl1n@users.noreply.github.com>
2023-06-28 14:43:12 -0300

    Update weather.js
    Fix error on baseUrl

--- js/weather.js
+++ js/weather.js
@@ -1,5 +1,5 @@
 const APIKey = 'GvFVnOD2Z9hzOxKGGZKf5pFuVoAdlJmB';
-const baseUrl = 'http://dataservice.accuweather.com/';
+const baseUrl = 'https://dataservice.accuweather.com/';
 
 const getCityUrl = (cityName) =>
   `${baseUrl}locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`;
